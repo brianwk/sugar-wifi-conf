41a6fc8b837cf8c6d2e0ad9d2ec7821b7c6dfd7a
Brian Kaplan <brian@softwaredeployed.com>
2020-04-06 03:38:03 -0500

    fill out stub for onReadRequest

--- characteristics/workout-routine.js
+++ characteristics/workout-routine.js
@@ -13,15 +13,21 @@ let WorkoutRoutineCharacteristic = function () {
 
 util.inherits(WorkoutRoutineCharacteristic, BlenoCharacteristic)
 
-WorkoutRoutineCharacteristic.prototype.onReadRequest = function (result, data) {
-    console.log('workout routine read', result, data)
+WorkoutRoutineCharacteristic.prototype.onReadRequest = function (offset, callback) {
+    console.log('workout routine read', offset)
+    /**
+     * @app.route('/set_workout_routine/<id>')
+     * @app.route('/workouts_available')
+     */
+    const data = '["1", "2", "3", "4", "5"]'
+    // @todo example JSON data which represents workout IDs
+    callback(this.RESULT_SUCCESS, data)
 }
 
 WorkoutRoutineCharacteristic.prototype.onWriteRequest = function (data, offset, withoutResponse, callback) {
     console.log('InputCharacteristic write request: ' + data.toString() + ' ' + offset + ' ' + withoutResponse)
     /**
      * @app.route('/set_workout_routine/<id>')
-     * @app.route('/workouts_available')
      */
     let workoutId = data.toString()
     console.log('got workoutId', workoutId)

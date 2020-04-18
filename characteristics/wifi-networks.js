6ddb68f0ec3ecf82023b5769d6a01bc6ae14e2a6
Brian Kaplan <brian@softwaredeployed.com>
2020-04-18 02:03:14 -0500

    update handler

--- characteristics/wifi-networks.js
+++ characteristics/wifi-networks.js
@@ -17,13 +17,13 @@ let WifiNetworksCharacteristic = function () {
 
 util.inherits(WifiNetworksCharacteristic, JsonObjectCharacteristic)
 
-WifiNetworksCharacteristic.prototype.onObjectUpdate = function () {
-    WifiNetworksCharacteristic.prototype.onObjectUpdate.super_.apply(this, [maxValueSize, updateValueCallback])
+WifiNetworksCharacteristic.prototype.onSubscribe = function () {
+    WifiNetworksCharacteristic.prototype.onSubscribe.super_.apply(this, [maxValueSize, updateValueCallback])
 
     wlan0.on('ready', () => wlan0.scan())
     wlan0.on(
         'update',
-        this.onObjectUpdate.bind(this)
+        this.onUpdateObject.bind(this)
     )
 }
 

3df114523fbcf8116372a3da6f0244bdf2b84c1b
Brian Kaplan <brian@equalslashed.net>
2020-04-20 01:14:15 +0100

    latest updates

--- characteristics/json-object.js
+++ characteristics/json-object.js
@@ -1,8 +1,6 @@
 let util = require('util')
 let bleno = require('bleno')
 let UUID = require('../sugar-uuid')
-let wpa = require('wpa_supplicant')
-const wlan0 = wpa('wlan0')
 const { Observable } = require('rxjs')
 
 let BlenoCharacteristic = bleno.Characteristic
@@ -11,6 +9,7 @@ let JsonObjectCharacteristic = function (serviceUuid) {
     if (!serviceUuid) {
         throw new Error("You must supply serviceUuid argument as a string to instantiate this Characteristic")
     }
+    console.log('serviceUuid', serviceUuid)
     JsonObjectCharacteristic.super_.call(this, {
         uuid: serviceUuid,
         properties: ['notify']
@@ -26,8 +25,9 @@ JsonObjectCharacteristic.prototype.emitObject = function (object) {
 JsonObjectCharacteristic.prototype.onObjectUpdate = function () {
     const next = function (object) {
         const encoded = JSON.stringify(object)
-        try {
-            const buffer = Buffer.from(encoded, 'ascii')
+        let buffer
+	try {
+            buffer = Buffer.from(encoded, 'ascii')
         } catch (e) {
             console.log('Invalid buffer', encoded)
         }
@@ -36,13 +36,15 @@ JsonObjectCharacteristic.prototype.onObjectUpdate = function () {
         }
     }.bind(this)
 
+    let self = this
     const objects = Observable.create((observer) => {
-        this.iterateObjects(observer)
+	self.observer = observer
+	self.iterateObjects(self.emitObject)
         observer.complete()
     })
 
     objects
-        .pipe.apply(objects, this.pipeline)
+        .pipe.apply(objects, this.pipeline.call(this))
         .subscribe({ next })
 }
 
--- characteristics/wifi-networks.js
+++ characteristics/wifi-networks.js
@@ -7,28 +7,32 @@ const JsonObjectCharacteristic = require('./json-object')
 
 let WifiNetworksCharacteristic = function () {
     WifiNetworksCharacteristic.super_.call(this, UUID.WIFI_NETWORKS)
-    this.pipeline = [
+    this.pipeline = () => (
+	[
         distinct(({ ssid }) => ssid),
-        map(({ ssid, signal }) => {
-            return { ssid, signal }
-        })
-    ]
+        map(({ ssid, signal }) => ( { ssid, signal }))
+	]
+    )
 }
 
 util.inherits(WifiNetworksCharacteristic, JsonObjectCharacteristic)
 
-WifiNetworksCharacteristic.prototype.onSubscribe = function () {
-    WifiNetworksCharacteristic.prototype.onSubscribe.super_.apply(this, [maxValueSize, updateValueCallback])
+WifiNetworksCharacteristic.prototype.iterateObjects = function(callback) {
+    return wlan0.networks.forEach(callback.bind(this))
+}
 
-    wlan0.on('ready', () => wlan0.scan())
+WifiNetworksCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+    WifiNetworksCharacteristic.super_.prototype.onSubscribe.apply(this, [maxValueSize, updateValueCallback])
+
+    wlan0.on('ready', () => { console.log('scan'); wlan0.scan(); })
     wlan0.on(
         'update',
-        this.onUpdateObject.bind(this)
+        this.onObjectUpdate.bind(this)
     )
 }
 
 WifiNetworksCharacteristic.prototype.onNotify = function () {
-    wlan0.scan()
+//    wlan0.scan()
 }
 
 WifiNetworksCharacteristic.prototype.onUnsubscribe = function () {
